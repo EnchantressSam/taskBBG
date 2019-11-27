e1fbbc0167cf41142cf53c313bef947aad1c7ae3
Tatyana Loginova <tatyanka8samara@mail.ru>
2019-11-27 19:43:06 +0400

    added scroll menu & scroll button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  /*portfolio*/
   $(".button[filter]").click(function() {
     if ($(this).attr("val") == "off") {
       $(".button[filter]")
@@ -19,13 +20,48 @@ $(document).ready(function() {
         $(".filter > div").show(300);
       }
     }
-  })
-  $('.multiple-items').slick({
+  });
+  /*slider*/
+  $(".multiple-items").slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
     arrows: false,
     dots: true,
-    dotsClass: 'slick-dots'
+    dotsClass: "slick-dots",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  });
+  /*menu*/
+  $('ul.menu a[href^="#"').click(function() {
+    var target = $(this).attr('href');
+    $('html, body').animate(
+      {
+        scrollTop: $(target).offset().top
+      },
+      500
+    );
+    $('ul.menu a[href^="#"').css({ 'color': '#212121' });
+    $(this).css({ 'color': '#004bee' });
+    return false;
+  });
+  /*up botton*/
+  $(window).scroll(function(){
+    if ($(this).scrollTop() != 0)
+    $('#toTop').fadeIn();
+    else
+    $('#toTop').fadeOut();
+  });
+  $('#toTop').click(function(){
+    $('body, html').animate({
+      scrollTop: 0
+    }, 800);
   });
 });
